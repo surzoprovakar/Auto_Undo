8308a81b2d8af46c4a987301ccaa6b675ef49129
provakar <surzoprovakar>
2023-05-30 21:49:41 -0400

    fix proxy methods not changing original class values

--- IIM.js
+++ IIM.js
@@ -2,7 +2,7 @@ var Counter = require('./counter')
 
 var is_undoing = false
 
-function read_json(filename)  {
+function read_json(filename) {
     var file = require(filename)
     return file.functions
 }
@@ -13,42 +13,44 @@ function generateUndoAction(funcName, param) {
 }
 
 var metaData = read_json('./metadata.json')
-var origInc = Counter.prototype.inc;
-Counter.prototype.inc = function(value) {
-    console.log("calling proxied inc")
-    if (!is_undoing) {
-        generateUndoAction("inc", value)
-    }
-    origInc.apply(Counter, [value]);
+//console.log(metaData)
+
+// var origInc = Counter.prototype.inc
+// Counter.prototype.inc = function (value) {
+//     console.log("calling proxied inc")
+//     if (!is_undoing) {
+//         generateUndoAction("inc", value)
+//     }
+//     origInc.apply(Counter, [value]);
+// }
+
+var funcs = []
+for (var func of metaData) {
+    var funcName = func.name
+    var funcParam = func.param
+    //console.log(funcName, " ", funcParam) 
+    var patchedFunc = "var origfunc = Counter.prototype." + funcName + "\n\
+        Counter.prototype." + funcName + " = function (" + funcParam + ") {\n\
+        console.log(\"calling proxied " + funcName + "\")\n\
+        if (!is_undoing) {\n\
+        generateUndoAction(" + "\"" + funcName + "\"" + ", " + funcParam + ")\n\
+        }\n\
+        origfunc.apply(this, [" + funcParam + "])\n\
+        }\n"
+    funcs.push(patchedFunc)
+    //eval(patchedFunc)
+    //console.log(patchedFunc)
 }
+
+//console.log(funcs)
+funcs.forEach((func) => {
+    eval(func)
+})
 
 let c1 = new Counter("pn")
+c1.inc(4)
+c1.dec(1)
+c1.print()
+c1.dec(5)
 c1.inc(1)
-
-//console.log(metaData)
-/*
-for (var func of metaData) {
-    //var func = metaData[i]
-    var funcName = func.name 
-    var funcParam = func.param 
-    //console.log(funcName, " ", funcParam) 
-
-    var patchedFunc = "var patched"+funcName+ "= function(gregory) {\n\
-        \tif (!is_undoing) {\n\
-        \t\tgenerateUndoAction(\"inc\", \"gregory\")\n\
-        \t}\n\
-        \treturn Counter." + funcName +"(" + funcParam + ")\n\
-        \t}"
-    eval(patchedFunc)
-    
-    console.log(patchedFunc)
-}
-*/
-
-// var patchedFunc = function(gregory) {
-//     //////
-//     if (!is_undoing) {
-//         generateUndoAction("inc", "gregory")
-//     }
-//     return Counter.inc(gregory)
-// }
+c1.print()
