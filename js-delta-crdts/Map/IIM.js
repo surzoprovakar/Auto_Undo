4025d517bb81645ae937712285830ad93eb9ff5e
surzoprovakar <provakar@vt.edu>
2023-06-14 17:47:35 -0400

    generate patched codes for the map crdt's update functions depending on metadata

--- js-delta-crdts/Map/IIM.js
+++ js-delta-crdts/Map/IIM.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const { execSync } = require('child_process')
-var { add_key, modify_key, remove_key } = require('./app')
 
 CRDT = require('delta-crdts')
 map = CRDT('ormap')
 orm = map('m1')
 ms = orm.state().state
+
+var no_op
+var operations_history = []
+var funcs = []
 
 function read_crdts(filename) {
     var file = require(filename)
@@ -19,73 +22,105 @@ function read_functions(crdt) {
 var crdts = read_crdts('../MetaData/metaData.json')
 var functions = read_functions(crdts)
 var no_op = crdts[0].no_op
+var cond = crdts[0].cond
 
-function generate_patched() {
-    var origFunc = add_key
-    console.log(origFunc)
-    add_key = function (key, val) {
-        console.log("calling proxied add key")
-        origFunc.apply(this, [key, val])
-    }
-    console.log("here")
-}
+// console.log(functions)
+// console.log(no_op)
+// console.log(cond)
 
-
-/*
-function generateUndoAction(funcName, param, undo_func) {
+function generateUndoAction(funcName, param1, param2, undo_func, no_op) {
     if (no_op) {
         console.log("No Op detected")
         return
-    }
-    if (param != null) {
-        console.log("generating undo action for: " + funcName + ", " + param + "\n")
-        operations_history.push([undo_func, param])
     } else {
         console.log("generating undo action for: " + funcName + "\n")
-        operations_history.push([undo_func])
+        operations_history.push([undo_func, param1, param2])
     }
 }
 
 function generate_patched() {
-    for (var func of functions) {
-        var funcName = func.name
-        var funcParam = func.param
-        var undoFunc = func.rg
-        var patchedFunc = "var origfunc = ars." + funcName + "\n\
-        ars." + funcName + " = function (" + funcParam + ") {\n\
-        console.log(\"calling proxied " + funcName + "\")\n\
-        origfunc.apply(this, [" + funcParam + "])\n\
-        after_size =  ars.value().size\n\
-        if (is_undoing) {\n\
-        generateUndoAction(" + "\"" + funcName + "\"" + ", " + funcParam + "," + "\"" + undoFunc + "\"" + ")\n\
-        }\n\
-        }\n"
-        funcs.push(patchedFunc)
+
+    const uniqueFunctions = [...new Set(functions.map(item => item.name))]
+    // console.log(uniqueFunctions)
+    for (var func of uniqueFunctions) {
+        var patchedFunc
+        // console.log(func)
+        if (func == 'set') {
+            patchedFunc = "var origSet = ms." + func + "\n\
+            ms." + func + " = function (key, val) {\n\
+                console.log(\"calling proxied set\")\n\
+                if (eval(\"ms.\" + cond)) {\n\
+                    console.log(\"key already exists\")\n\
+                    var meta = functions.find((fn) =>  fn.cond === 'true')\n\
+                    var undoFunc = meta.rg\n\
+                    var param1 = key\n\
+                    var param2 = ms.get(key)\n\
+                    if (ms.get(key) == val) { no_op = true }\n\
+                    else { no_op = false }\n\
+                    generateUndoAction(" + "\"" + func + "\"" + ", param1, param2, undoFunc, no_op)\n\
+                    origSet.apply(this, [key, val])\n\
+                } else {\n\
+                    var meta = functions.find((fn) =>  fn.cond === 'false')\n\
+                    var undoFunc = meta.rg\n\
+                    var param = key\n\
+                    no_op = false\n\
+                    generateUndoAction(" + "\"" + func + "\"" + ", param, null, undoFunc, no_op)\n\
+                    origSet.apply(this, [key, val])\n\
+                }\n\
+            }\n"
+            funcs.push(patchedFunc)
+        }
+        else {
+            patchedFunc = "var origDel = ms." + func + "\n\
+            ms." + func + " = function (key) {\n\
+                console.log(\"calling proxied delete\")\n\
+                if (eval(\"ms.\" + cond)) {\n\
+                    no_op = false\n\
+                    var meta = functions.find((fn) => fn.name === " + "\"" + func + "\"" + ")\n\
+                    var undoFunc = meta.rg\n\
+                    var param1 = key\n\
+                    var param2 = ms.get(key)\n\
+                    origDel.apply(this, [key])\n\
+                } else {\n\
+                    no_op = true\n\
+                    console.log(\"key not in there\")\n\
+                }\n\
+                generateUndoAction(" + "\"" + func + "\"" + ", param1, param2, undoFunc, no_op)\n\
+            }\n"
+            funcs.push(patchedFunc)
+        }
     }
 }
+// console.log(operations_history)
+// console.log(funcs)
 
 function execute_patch() {
     //console.log(funcs)
     funcs.forEach((func) => {
         eval(func)
     })
-}*/
-
-
-module.exports = {
-    generate_patched
 }
 
+function execute_undo(ms) {
+    while (operations_history.length > 0) {
+        const [opName, key, val] = operations_history.pop()
 
-// function add_key(key, val) {
-//     console.log("origin")
-//     return key + val
-// }
+        if (opName == 'delete') { ms.delete(key) }
+        else { ms.set(key, val) }
 
-// var origFunc = add_key
-// add_key = function(key, val) {
-//     console.log("calling proxy")
-//     return origFunc.apply(this,[key, val])
-// }
+    }
+}
 
-// console.log(add_key(1,2))
+module.exports = {
+    generate_patched,
+    execute_patch,
+    operations_history
+}
+
+function is_no_op(map1, map2) {
+    if (map1.size !== map2.size) { return true }
+    for (let [key, value] of map1) {
+        if (!map2.has(key) || map2.get(key) !== value) { return true }
+    }
+    return false
+}
--- js-delta-crdts/Map/app.js
+++ js-delta-crdts/Map/app.js
@@ -8,68 +8,24 @@
 // s.delete('b')
 // console.log(s)
 
-const prompt = require('prompt-sync')()
 CRDT = require('delta-crdts')
 map = CRDT('ormap')
 orm = map('m1')
 ms = orm.state().state
-var no_op
+const { generate_patched,execute_patch, operations_history } = require('./IIM')
 
-//#region Wrapper for the Update methods
-function add_key(key, val) {
-    if (ms.has(key)) { modify_key(key, val) }
-    else {
-        ms.set(key, val)
-        no_op = false
-    }
-}
-
-function modify_key(key, val) {
-    if (val == ms.get(key)) {
-        no_op = true
-    } else {
-        console.log('modifying existing key')
-        ms.set(key, val)
-        no_op = false
-    }
-}
-
-function remove_key(key) {
-    if (ms.has(key)) {
-        ms.delete(key)
-        no_op = false
-    }
-    else {
-        console.log("key not present")
-        no_op = true
-    }
-}
-//#endregion
-
-module.exports = {
-    custom_undo_check,
-    add_key,
-    modify_key,
-    remove_key
-}
-
-function custom_undo_check() {
-    // users have the flexibility to add custom logics here rather than metaData
-    const input = prompt("Enter 0 or 1: ")
-    return input == 1 ? true : false
-}
-
-const { generate_patched, execute_patch, undoable } = require('./IIM')
 
 generate_patched()
+execute_patch()
 
-add_key('a', 1)
-add_key('b', 2)
-add_key('c', 1)
-console.log(ms)
-add_key('c', 4)
-console.log(ms)
+ms.set('a', 1)
+ms.set('b', 2)
+ms.set('c', 3)
+// console.log(ms)
+ms.set('a', 5)
+ms.delete('b')
+ms.delete('d')
+ms.set('a', 5)
+console.log(ms.entries())
 
-remove_key('a')
-remove_key('d')
-console.log(ms)
+console.log(operations_history)
